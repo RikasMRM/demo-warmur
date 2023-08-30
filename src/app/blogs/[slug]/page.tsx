a5b5c8cecfe95f8b6b2efdec3f72811edd63bdfc
Rikas <rikasrkf@gmail.com>
2023-08-30 22:12:15 +0530

    implemented strapi

---
+++ src/app/blogs/[slug]/page.tsx
@@ -0,0 +1,19 @@
+// import React from "react";
+// import SectionHeading from "@/components/section-heading";
+
+// import { getPost } from "@/services/postService";
+
+// export default async function Blogs({ params }: { id: { id: string } }) {
+//   const post: any = await getPost(params.id);
+//   console.log("aaaaaa",post);
+//   return (
+//     <>
+//       <div className="flex flex-col items-center justify-center h-screen">
+//         <SectionHeading>{post.title}</SectionHeading>
+//         <div>
+//           <div>{post.content}</div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
