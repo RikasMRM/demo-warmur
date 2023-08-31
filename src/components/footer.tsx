2af38a72fd9edec5be30cca261e3dfe9c1c0dcc4
Rikas <rikasrkf@gmail.com>
2023-08-31 20:55:54 +0530

    footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -10,53 +10,50 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="max-container bg-black w-full">
-      <div className="flex justify-between items-start gap-20 my-10 flex-wrap max-lg:flex-col">
-        <div className="flex flex-col items-start">
-          <Link href="/">
-            <Image
-              src={logo}
-              alt="logo"
-              width={150}
-              height={46}
-              className="m-0"
-            />
-          </Link>
+    <footer className="flex flex-col text-white w-full bg-primary-blue mt-5">
+      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+        <div className="flex flex-col justify-start items-start gap-6">
+          <Image
+            src={logo}
+            alt="logo"
+            width={118}
+            height={18}
+            className="object-contain"
+          />
+          <p className="text-base text-white">
+            Warmur 2023 <br />
+            All Rights Reserved &copy;
+          </p>
         </div>
 
-        <div className="flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap">
-          {footerLinks.map((section) => (
-            <div key={section.title}>
-              <h4 className="font-montserrat text-2xl leading-normal font-medium mb-6 text-white">
-                {section.title}
-              </h4>
-              <ul>
-                {section.links.map((link: any) => (
-                  <li
-                    className="mt-3 font-montserrat text-base leading-normal text-white-400 hover:text-slate-gray"
-                    key={link.name}
-                  >
-                    <Link href={link.link}>{link.name}</Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-
-        <div className="flex items-center gap-5 mt-8">
-          {socialMedia.map((icon) => (
+        <div className="flex-1 w-full flex md:justify-end flex-wrap max-md:mt-10 gap-20">
+          {footerLinks.map((item) => (
             <div
-              className="flex justify-center items-center w-12 h-12 bg-white rounded-full"
-              key={icon.alt}
+              key={item.title}
+              className="flex flex-col gap-6 text-white min-w-[170px]"
             >
-              <Image src={icon.src} alt={icon.alt} width={24} height={24} />
+              {/* <h3 className="font-bold">{item.title}</h3> */}
+              <div className="flex flex-col gap-5">
+                {item.links.map((link) => (
+                  <Link key={link.title} href={link.url} className="text-white">
+                    {link.title}
+                  </Link>
+                ))}
+              </div>
             </div>
           ))}
-        </div>
-
-        <div className="lg:max-w-[30%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
-          <div className="flex max-sm:justify-end items-center max-sm:w-full">
+          <div className="flex items-center gap-5 mt-8">
+            {socialMedia.map((icon) => (
+              <div
+                className="flex justify-center items-center w-8 h-8 bg-white rounded-full"
+                key={icon.alt}
+              >
+                <Image src={icon.src} alt={icon.alt} width={24} height={24} />
+              </div>
+            ))}
+          </div>
+          <div className="lg:max-w-[30%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
+            <div className="flex max-sm:justify-end items-center max-sm:w-full">Enter your email</div>
           </div>
         </div>
       </div>
